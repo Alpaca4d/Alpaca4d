7eefc28c8af03c75852522076f602ec7aaf325fb
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2021-11-02 22:39:11 +0000

    update

--- WIP_Files/forNextRelease/Timber/timberCheck.py
+++ WIP_Files/forNextRelease/Timber/timberCheck.py
@@ -1,241 +1,352 @@
 import math
+import json
+import os
 
-k_mod = {"Solid_Timber":{
-                    1:{"permanent":0.60,"long_term":0.70,"medium_term":0.80,"short_term":0.90,"instantaneous":1.10},
-                    2:{"permanent":0.60,"long_term":0.70,"medium_term":0.80,"short_term":0.90,"instantaneous":1.10},
-                    3:{"permanent":0.50,"long_term":0.55,"medium_term":0.65,"short_term":0.70,"instantaneous":0.90}},
-        "LVL":   {
-                    1:{"permanent":0.00,"long_term":0.00,"medium_term":0.00,"short_term":0.00,"instantaneous":0.00},
-                    2:{"permanent":0.00,"long_term":0.00,"medium_term":0.00,"short_term":0.00,"instantaneous":0.00},
-                    3:{"permanent":0.00,"long_term":0.00,"medium_term":0.00,"short_term":0.00,"instantaneous":0.00}}
-        }
-
-
-safety_factor = {"Solid_Timber":1.3,"Glulam":1.25,"LVL":1.2}
 
 # reference value in [N/mm2]
 
-timber_properties = {"GOB_Green_Oak":
-                                {"fm_k":27.8,"fv_k":4.01,"E_mean":9260,"E0_05":7130,"G_mean":590,"fc_0_k":17.2,"fc_90_k":6.1,"ft_0_k":0.00,"ft_90_k":0.00,"rho_k":000,"type":"solid Timber"},
-                    "C14":
-                                {"fm_k":14.0,"fv_k":3.00,"E_mean":7000,"E0_05":4700,"G_mean":440,"fc_0_k":16.0,"fc_90_k":2.0,"ft_0_k":8.00,"ft_90_k":0.40,"rho_k":290,"type":"solid Timber"},
-                    "C16":
-                                {"fm_k":16.0,"fv_k":1.80,"E_mean":8000,"E0_05":5400,"G_mean":500,"fc_0_k":17.0,"fc_90_k":2.2,"ft_0_k":10.0,"ft_90_k":0.50,"rho_k":310,"type":"solid Timber"},
-                    "Cxx":
-                                {"fm_k":0000,"fv_k":0000,"E_mean":0000,"E0_05":0000,"G_mean":000,"fc_0_k":0000,"fc_90_k":000,"ft_0_k":0000,"ft_90_k":0000,"rho_k":000,"type":"solid Timber"},
-                    "Cx1":
-                                {"fm_k":0000,"fv_k":0000,"E_mean":0000,"E0_05":0000,"G_mean":000,"fc_0_k":0000,"fc_90_k":000,"ft_0_k":0000,"ft_90_k":0000,"rho_k":000,"type":"solid Timber"}
-                    }
-
-
-straightness_factor = {"Solid_Timber":0.2,"Glulam":0.1,"LVL":0.1}
-
-
 # Input [mm]
 
-b = 75
-h = 15
+# b = 75
+# h = 15
 
-L_ey = 1000
-L_ez = 1000
+# L_ey = 1000
+# L_ez = 1000
 
-material_type = "Solid_Timber"
-material_name = "GOB_Green_Oak"
-service_class = 2
-duration = "instantaneous"
 
-# Units [N]
+# Timber_Class = "C22"
+# service_class = 1
+# duration = "instantaneous"
 
-N = -4000
-M_y = 100000
-M_z = 100000
-V = 1000
+# # Units [N]
+
+# N_x = -4000
+# V_y = 1000
+# V_z = 1000
+# M_x = 1000
+# M_y = 100000
+# M_z = 100000
+
+
+
+# Opening JSON
+def load_timber_properties(folder_path):
+
+    collected_properties = []
+
+    properties = ['timber_properties.json','k_mod.json','safety_factor.json','straightness_factor.json']
+
+    for properti in properties:
+
+        file_path = os.path.join(folder_path, properti)
+        with open(file_path, 'r') as json_file:
+            collected_properties.append(json.load(json_file))
+    
+    timber_properties, k_mod, safety_factor, straightness_factor = collected_properties
+
+    return timber_properties, k_mod, safety_factor, straightness_factor
 
 
 # Section Properties
+def section_properties(b, h):
 
-Area = b * h
+        Area = b * h
 
-W_y = (b * h**2) / 6
-W_z = (b**2 * h) / 6
+        W_y = (b * h**2) / 6
+        W_z = (b**2 * h) / 6
 
+        I_y = (b * h**3) / 12
+        I_z = (b**3 * h) / 12
 
-I_y = (b * h**3) / 12
-I_z = (b**3 * h) / 12
-I_t = 1/3 * b**3 * h * (1-0.63*(b/h))
+        I_t = 1/3 * b**3 * h * (1-0.63*(b/h))
 
-radii_of_gyration_y = (I_y/Area)**0.5
-radii_of_gyration_z = (I_z/Area)**0.5
+        radii_of_gyration_y = (I_y/Area)**0.5
+        radii_of_gyration_z = (I_z/Area)**0.5
+
+        return Area, W_y, W_z, I_y, I_z, I_t, radii_of_gyration_y, radii_of_gyration_z
 
 
 # Material Properties
+def material_properties(timber_properties, Timber_Class):
 
-fm_k = timber_properties[material_name]["fm_k"]
-fv_k = timber_properties[material_name]["fv_k"]
-fc_0_k = timber_properties[material_name]["fc_0_k"]
-ft_0_k = timber_properties[material_name]["ft_0_k"]
-E_mean = timber_properties[material_name]["E_mean"]
-E0_05 = timber_properties[material_name]["E0_05"]
-G_mean = timber_properties[material_name]["G_mean"]
-G0_05 = E0_05/16
+        material_type = timber_properties[Timber_Class]["type"]
+        fm_k = timber_properties[Timber_Class]["fm_k"]
+        fv_k = timber_properties[Timber_Class]["fv_k"]
+        fc_0_k = timber_properties[Timber_Class]["fc_0_k"]
+        ft_0_k = timber_properties[Timber_Class]['ft_0_k']
+        E_mean = timber_properties[Timber_Class]["E_mean"]
+        E0_05 = timber_properties[Timber_Class]["E0_05"]
+        G_mean = timber_properties[Timber_Class]["G_mean"]
+        G0_05 = E0_05/16
+
+        return material_type, fm_k, fv_k, fc_0_k, ft_0_k, E_mean, E0_05, G_mean, G0_05
+
 
 
 # Design Factor
+def design_parameters(b, h, safety_factor, material_type, k_mod, service_class, duration):
 
-gamma_m = safety_factor[material_type]
-kmod = k_mod[material_type][service_class][duration]
+    km = 0.70
+    gamma_m = safety_factor[material_type]
+    kmod = k_mod[material_type][service_class][duration]
 
-
-ky_h = min( (150/h)**0.2 , 1.3 )
-kz_h = min( (150/b)**0.2 , 1.3 )
-
-ky_h = 1.0 if ky_h < 1.0 else ky_h
-kz_h = 1.0 if kz_h < 1.0 else kz_h
-
-print("ky_h: {}".format(ky_h))
-print("kz_h: {}".format(kz_h))
+    print("kmod: {0:.2f}".format(kmod))
 
 
-# Biaxial bending factor
-km = 0.70
+    ky_h = min( (150/h)**0.2 , 1.3 )
+    kz_h = min( (150/b)**0.2 , 1.3 )
+
+    ky_h = 1.0 if ky_h < 1.0 else ky_h
+    kz_h = 1.0 if kz_h < 1.0 else kz_h
+
+    print("ky_h: {0:.2f}".format(ky_h))
+    print("kz_h: {0:.2f}".format(kz_h))
 
 
-lambda_y = L_ey / radii_of_gyration_y
-lambda_z = L_ez / radii_of_gyration_z
+    kh = min(ky_h,kz_h)
 
-print("lambda_y: {}".format(lambda_y))
-print("lambda_z: {}".format(lambda_z))
+    print("kh: {0:.2f}".format(kh))
 
-
-lambda_rel_y = (lambda_y/math.pi) * (fc_0_k/E0_05)**0.5
-lambda_rel_z = (lambda_z/math.pi) * (fc_0_k/E0_05)**0.5
-
-print("lambda_rel_y: {}".format(lambda_rel_y))
-print("lambda_rel_z: {}".format(lambda_rel_z))
+    return km, gamma_m , kmod, ky_h, kz_h, kh
 
 
-beta_c = straightness_factor[material_type]
+def buckling_parameters(b, h, L_ey, L_ez, radii_of_gyration_y, radii_of_gyration_z, fm_k, fc_0_k, E0_05, straightness_factor, material_type):
+    
+
+    lambda_y = L_ey / radii_of_gyration_y
+    lambda_z = L_ez / radii_of_gyration_z
+
+    print("lambda_y: {0:.2f}".format(lambda_y))
+    print("lambda_z: {0:.2f}".format(lambda_z))
 
 
-k_y = 0.5 * (1 + beta_c * (lambda_rel_y - 0.3) + lambda_rel_y**2)
-k_z = 0.5 * (1 + beta_c * (lambda_rel_z - 0.3) + lambda_rel_z**2)
+    lambda_rel_y = (lambda_y/math.pi) * (fc_0_k/E0_05)**0.5
+    lambda_rel_z = (lambda_z/math.pi) * (fc_0_k/E0_05)**0.5
 
-print("k_y: {}".format(k_y))
-print("k_z: {}".format(k_z))
-
-
-kc_y = 1 / (k_y + math.sqrt(k_y**2 - lambda_rel_y**2))
-kc_z = 1 / (k_z + math.sqrt(k_z**2 - lambda_rel_z**2))
-
-print("kc_y: {}".format(kc_y))
-print("kc_z: {}".format(kc_z))
+    print("lambda_rel_y: {0:.2f}".format(lambda_rel_y))
+    print("lambda_rel_z: {0:.2f}".format(lambda_rel_z))
 
 
-kc = min(kc_y,kc_z)
+    beta_c = straightness_factor[material_type]
 
-print("kc: {}".format(kc))
+    k_y = 0.5 * (1 + beta_c * (lambda_rel_y - 0.3) + lambda_rel_y**2)
+    k_z = 0.5 * (1 + beta_c * (lambda_rel_z - 0.3) + lambda_rel_z**2)
 
-#update formula to check b < h
-
-if b > h:
-        b_temp = h
-        h_temp = b
-sigma_m_crit = E0_05 * (0.78 * b_temp**2) / (h_temp * L_ey)
-lambda_rel_m = math.sqrt(fm_k/sigma_m_crit)
-
-print("sigma_m_crit: {}".format(sigma_m_crit))
-print("lambda_rel_m: {}".format(lambda_rel_m))
+    print("k_y: {0:.2f}".format(k_y))
+    print("k_z: {0:.2f}".format(k_z))
 
 
-if lambda_rel_m < 0.75:
-        k_crit = 1.00
-elif lambda_rel_m <= 1.40:
-        k_crit = 1.56-0.75*lambda_rel_m
-elif lambda_rel_m > 1.40:
-        k_crit = 1/lambda_rel_m**2
+    kc_y = 1 / (k_y + math.sqrt(k_y**2 - lambda_rel_y**2))
+    kc_z = 1 / (k_z + math.sqrt(k_z**2 - lambda_rel_z**2))
 
-print("k_crit: {}".format(k_crit))
-print("\n")
+    print("kc_y: {0:.2f}".format(kc_y))
+    print("kc_z: {0:.2f}".format(kc_z))
+
+
+    kc = min(kc_y,kc_z)
+
+    print("kc: {0:.2f}".format(kc))
+
+
+    sigma_m_crit_y = E0_05 * (0.78 * b**2) / (h * L_ey)
+    lambda_rel_m_y = math.sqrt(fm_k/sigma_m_crit_y)
+
+    sigma_m_crit_z = E0_05 * (0.78 * h**2) / (b * L_ez)
+    lambda_rel_m_z = math.sqrt(fm_k/sigma_m_crit_z)
+
+
+    print("sigma_m_crit_y: {0:.2f}".format(sigma_m_crit_y))
+    print("lambda_rel_m_y: {0:.2f}".format(lambda_rel_m_y))
+    print("sigma_m_crit_z: {0:.2f}".format(sigma_m_crit_z))
+    print("lambda_rel_m_z: {0:.2f}".format(lambda_rel_m_z))
+
+
+    if lambda_rel_m_y < 0.75:
+            k_crit_y = 1.00
+    elif lambda_rel_m_y <= 1.40:
+            k_crit_y = 1.56-0.75*lambda_rel_m_y
+    elif lambda_rel_m_y > 1.40:
+            k_crit_y = 1/lambda_rel_m_y**2
+
+
+    if lambda_rel_m_z < 0.75:
+            k_crit_z = 1.00
+    elif lambda_rel_m_z <= 1.40:
+            k_crit_z = 1.56-0.75*lambda_rel_m_z
+    elif lambda_rel_m_z > 1.40:
+            k_crit_z = 1/lambda_rel_m_z**2
+
+    print("k_crit_y: {0:.2f}".format(k_crit_y))
+    print("k_crit_z: {0:.2f}".format(k_crit_z))
+    print("\n")
+
+    return kc_y, kc_z, kc, k_crit_y, k_crit_z
+
+
 
 # Design Strength
-if N < 0.00:
-        sigma_c_0_d = N / Area
-        sigma_t_0_d = 0.00
-        print("sigma_c_0_d: {}".format(sigma_c_0_d))
-        print("sigma_t_0_d: {}".format(sigma_t_0_d))
-elif N > 0.00:
-        sigma_c_0_d = 0.00
-        sigma_t_0_d = N / Area
-        print("sigma_c_0_d: {}".format(sigma_c_0_d))
-        print("sigma_t_0_d: {}".format(sigma_t_0_d))
+
+def axial_stress(N_x, Area):
+    if N_x < 0.00:
+            sigma_c_0_d = abs(N_x) / Area
+            sigma_t_0_d = 0.00
+            print("sigma_c_0_d: {0:.2f}".format(sigma_c_0_d))
+            print("sigma_t_0_d: {0:.2f}".format(sigma_t_0_d))
+    elif N_x > 0.00:
+            sigma_c_0_d = 0.00
+            sigma_t_0_d = abs(N_x) / Area
+            print("sigma_c_0_d: {0:.2f}".format(sigma_c_0_d))
+            print("sigma_t_0_d: {0:.2f}".format(sigma_t_0_d))
+    else:
+            sigma_c_0_d = 0.00
+            sigma_t_0_d = 0.00
+            print("sigma_c_0_d: {0:.2f}".format(sigma_c_0_d))
+            print("sigma_t_0_d: {0:.2f}".format(sigma_t_0_d))
     
-sigma_m_y_d = M_y / W_y
-print("sigma_m_y_d: {}".format(sigma_m_y_d))
+    return sigma_c_0_d, sigma_t_0_d
 
-sigma_m_z_d = M_z / W_z
-print("sigma_m_z_d: {}".format(sigma_m_z_d))
 
-k_cr = 0.67
-Area_eff = b * k_cr * h
-tau_d = 1.5 * V / Area_eff
-print("tau_d: {}".format(tau_d))
+def bending_stress(M_y, M_z, W_y, W_z):
+    
+    sigma_m_y_d = abs(M_y) / W_y
+    sigma_m_z_d = abs(M_z) / W_z
+    
+    print("sigma_m_y_d: {0:.2f}".format(sigma_m_y_d))
+    print("sigma_m_z_d: {0:.2f}".format(sigma_m_z_d))
 
-fc_0_d = kmod * fc_0_k / gamma_m
-print("fc_0_d: {}".format(fc_0_d))
+    return sigma_m_y_d, sigma_m_z_d
 
-fm_y_d = kmod * ky_h * fm_k / gamma_m
-print("fm_y_d: {}".format(fm_y_d))
 
-fm_z_d = kmod * kz_h * fm_k / gamma_m
-print("fm_z_d: {}".format(fm_z_d))
+def shear_stress(V_y, V_z, b, h):
 
-fv_d = kmod * fv_k / gamma_m
-print("fv_d: {}".format(fv_d))
+    k_cr = 0.67
+    Area_eff = b * k_cr * h
+    tau_y_d = 1.5 * abs(V_y) / Area_eff
+    tau_z_d = 1.5 * abs(V_z) / Area_eff
+
+    tau_d = math.sqrt(tau_y_d**2 + tau_z_d**2)
+
+    print("tau_y_d: {0:.2f}".format(tau_y_d))
+    print("tau_z_d: {0:.2f}".format(tau_z_d))
+    print("tau_d: {0:.2f}".format(tau_d))
+
+    return tau_d
+
+
+def axial_resistance(kmod, fc_0_k, ft_0_k, gamma_m):
+
+    fc_0_d = kmod * fc_0_k / gamma_m
+    ft_0_d = kh * kmod * ft_0_k / gamma_m
+
+    print("fc_0_d: {0:.2f}".format(fc_0_d))
+    print("ft_0_d: {0:.2f}".format(ft_0_d))
+
+    return fc_0_d, ft_0_d
+
+
+def bending_resistance(kmod, ky_h, kz_h, fm_k, gamma_m):
+
+    fm_y_d = kmod * ky_h * fm_k / gamma_m
+    fm_z_d = kmod * kz_h * fm_k / gamma_m
+
+    print("fm_y_d: {0:.2f}".format(fm_y_d))
+    print("fm_z_d: {0:.2f}".format(fm_z_d))
+    return fm_y_d, fm_z_d
+
+
+def shear_resistance(kmod, fv_k, gamma_m):
+
+    fv_d = kmod * fv_k / gamma_m
+    print("fv_d: {0:.2f}".format(fv_d))
+
+    return fv_d
+
+
 
 # Design Combination
 
-if N > 0.0:
-        util_bendind_tens_y = (sigma_t_0_d/fc_0_d) + (sigma_m_y_d/fm_y_d) + kmod * (sigma_m_z_d/fm_z_d)
-        util_bending_tens_z = (sigma_t_0_d/fc_0_d) + kmod * (sigma_m_y_d/fm_y_d) + (sigma_m_z_d/fm_z_d)
-        util_bendin_comp_y = 0.00
-        util_bendin_comp_z = 0.00
-        util_LTB_capacity = (sigma_m_y_d-sigma_t_0_d)/(k_crit*fm_y_d) + km * sigma_m_z_d/fm_z_d
-else:
-        util_bendind_tens_y = 0.00
-        util_bending_tens_z = 0.00
-        util_bendin_comp_y = (abs(sigma_c_0_d)/(kc_y * fc_0_d)) + (sigma_m_y_d/fm_y_d) + km * (sigma_m_z_d/fm_z_d)
-        util_bendin_comp_z = (abs(sigma_c_0_d)/(kc_z * fc_0_d)) + km * (sigma_m_y_d/fm_y_d) + (sigma_m_z_d/fm_z_d)
-        util_LTB_capacity = (sigma_m_y_d/ (k_crit * fm_y_d))**2 + (abs(sigma_c_0_d)/(kc_z*fc_0_d))
+def utilisation_axial(sigma_c_0_d, sigma_t_0_d, ft_0_d, fc_0_d, kc):
+    
+    util_axial_tens = sigma_t_0_d/ft_0_d
+    util_axial_comp = abs(sigma_c_0_d/(kc*fc_0_d))
+    util_axial = max(util_axial_tens,util_axial_comp)
+
+    return util_axial_tens, util_axial_comp, util_axial
 
 
-# Summary
+def utilisation_bending(sigma_m_y_d, sigma_m_z_d, fm_y_d, fm_z_d, km):
 
-if sigma_c_0_d >= 0.0:
-        util_axial = sigma_c_0_d/fc_0_d
-elif sigma_c_0_d < 0:
-        util_axial = sigma_c_0_d/(kc*fc_0_d)
+    util_bending_y_1 = sigma_m_y_d/fm_y_d
+    util_bending_y_2 = km * sigma_m_y_d/fm_y_d
 
-util_bending_y_1 = sigma_m_y_d/fm_y_d
-util_bending_y_2 = kmod * sigma_m_y_d/fm_y_d
+    util_bending_z_1 = sigma_m_z_d/fm_z_d
+    util_bending_z_2 = km * sigma_m_z_d/fm_z_d
 
-util_bending_z_1 = sigma_m_z_d/fm_z_d
-util_bending_z_2 = kmod * sigma_m_z_d/fm_z_d
+    util_bending_y = max(util_bending_y_1,util_bending_y_2)
+    util_bending_z = max(util_bending_z_1,util_bending_z_2)
 
-util_bending_y = min(util_bending_y_1,util_bending_y_2)
-util_bending_z = min(util_bending_z_1,util_bending_z_2)
+    return util_bending_y, util_bending_z
 
-util_shear = tau_d/fv_d
+def utilisation_shear(tau_d, fv_d):
+
+    util_shear = tau_d/fv_d
+
+    return util_shear
 
 
-print("\n")
-print("Util Axial: {}".format(abs(round(util_axial,2))))
-print("Util Bending y: {}".format(abs(round(util_bending_y,2))))
-print("Util Bending z: {}".format(abs(round(util_bending_z,2))))
-print("Util Shear: {}".format(abs(round(util_shear,2))))
+def utilisation_tension_bending(sigma_t_0_d, ft_0_d, sigma_m_y_d, fm_y_d, sigma_m_z_d, fm_z_d, km):
+    
+    util_bending_tens_y = (sigma_t_0_d/ft_0_d) + (sigma_m_y_d/fm_y_d) + km * (sigma_m_z_d/fm_z_d)
+    util_bending_tens_z = (sigma_t_0_d/ft_0_d) + km * (sigma_m_y_d/fm_y_d) + (sigma_m_z_d/fm_z_d)
 
-print("\n")
-print("Util Combined Bending_y Tension: {}".format(abs(round(util_bendind_tens_y,2))))
-print("Util Combined Bending_z Tension: {}".format(abs(round(util_bending_tens_z,2))))
-print("Util Combined Bending_y Compression: {}".format(abs(round(util_bendin_comp_y,2))))
-print("Util Combined Bending_z Compression: {}".format(abs(round(util_bendin_comp_z,2))))
-print("Util Lateral Buckling: {}".format(abs(round(util_LTB_capacity,2))))
+    return util_bending_tens_y, util_bending_tens_z
+
+
+def utilisation_compression_bending(sigma_c_0_d, fc_0_d, sigma_m_y_d, fm_y_d, sigma_m_z_d, fm_z_d, kc_y, kc_z, km):
+
+    util_bending_comp_y_1 = (abs(sigma_c_0_d)/(kc_y * fc_0_d)) + (sigma_m_y_d/fm_y_d) + km * (sigma_m_z_d/fm_z_d)
+    util_bending_comp_y_2 = (abs(sigma_c_0_d)/(fc_0_d))**2 + (sigma_m_y_d/fm_y_d) + km * (sigma_m_z_d/fm_z_d)
+    util_bending_comp_z_1 = (abs(sigma_c_0_d)/(kc_z * fc_0_d)) + km * (sigma_m_y_d/fm_y_d) + (sigma_m_z_d/fm_z_d)
+    util_bending_comp_z_2 = (abs(sigma_c_0_d)/(fc_0_d))**2 + km * (sigma_m_y_d/fm_y_d) + (sigma_m_z_d/fm_z_d)
+        
+        
+    util_bending_comp_y = max(util_bending_comp_y_1,util_bending_comp_y_2)
+    util_bending_comp_z = max(util_bending_comp_z_1,util_bending_comp_z_2)
+
+    return util_bending_comp_y, util_bending_comp_z
+
+
+def utilisation_LTB(b, h, sigma_c_0_d, fc_0_d, sigma_t_0_d, sigma_m_y_d, fm_y_d, sigma_m_z_d, fm_z_d, k_crit_y, k_crit_z, kc_z, kc_y, km):
+
+    util_LTB_capacity_y = (sigma_m_y_d-sigma_t_0_d)/(k_crit_y*fm_y_d) + km * sigma_m_z_d/fm_z_d
+    util_LTB_capacity_z = (sigma_m_z_d-sigma_t_0_d)/(k_crit_z*fm_z_d) + km * sigma_m_y_d/fm_y_d
+
+    util_LTB_capacity_y = (sigma_m_y_d/ (k_crit_y * fm_y_d))**2 + (abs(sigma_c_0_d)/(kc_z*fc_0_d))
+    util_LTB_capacity_z = (sigma_m_z_d/ (k_crit_z * fm_z_d))**2 + (abs(sigma_c_0_d)/(kc_y*fc_0_d))
+
+    if h >= b:
+        util_LTB_capacity = util_LTB_capacity_y
+    else:
+        util_LTB_capacity = util_LTB_capacity_z
+
+    return util_LTB_capacity
+
+
+
+# utilisation = [util_axial, util_bending_y, util_bending_z, util_shear, util_bending_tens_y, util_bending_tens_z, util_bending_comp_y, util_bending_comp_z, util_LTB_capacity]
+# utilisation = [round(number,2) for number in utilisation]
+
+
+
+# print("\n")
+# print("Util Axial: {}".format(abs(round(util_axial,2))))
+# print("Util Bending y: {}".format(abs(round(util_bending_y,2))))
+# print("Util Bending z: {}".format(abs(round(util_bending_z,2))))
+# print("Util Shear: {}".format(abs(round(util_shear,2))))
+
+# print("\n")
+# print("Util Combined Bending_y Tension: {}".format(abs(round(util_bending_tens_y,2))))
+# print("Util Combined Bending_z Tension: {}".format(abs(round(util_bending_tens_z,2))))
+# print("Util Combined Bending_y Compression: {}".format(abs(round(util_bending_comp_y,2))))
+# print("Util Combined Bending_z Compression: {}".format(abs(round(util_bending_comp_z,2))))
+# print("Util Lateral Buckling: {}".format(abs(round(util_LTB_capacity,2))))
