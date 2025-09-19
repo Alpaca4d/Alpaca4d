e3de27948da97181fcc394e9a176e4d17c81f31d
MP <pellegrino.marco@icloud.com>
2025-09-19 16:32:30 +0200

    .

---
+++ Alpaca4d.Gh/00_Material/Uniaxial/UniaxialElastic.cs
@@ -0,0 +1,76 @@
+﻿using Alpaca4d.UIWidgets;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using Grasshopper.Kernel.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpaca4d.Gh
+{
+    internal class UniaxialElastic : SubComponent
+    {
+        public override string name() => "UniaxialElastic";
+        public override string display_name() => "UniaxialElastic";
+
+        public override void registerEvaluationUnits(EvaluationUnitManager mngr)
+        {
+            EvaluationUnit evaluationUnit = new EvaluationUnit(name(), display_name(), "");
+            evaluationUnit.Icon = Alpaca4d.Gh.Properties.Resources.uniaxial_Elastic_Material__Alpaca4d_;
+            mngr.RegisterUnit(evaluationUnit);
+
+
+            evaluationUnit.RegisterInputParam(new Param_String(), "Material Name", "MatName", "", GH_ParamAccess.item, new GH_String(""));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "E", "E", $"Young Modulus [{Units.Force}/{Units.Length}²]", GH_ParamAccess.item, new GH_Number(210000000));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "Eneg", "Eneg", $"[{Units.Force}/{Units.Length}²]", GH_ParamAccess.item, new GH_Number(210000000));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "Eta", "Eta", "", GH_ParamAccess.item, new GH_Number(0.0));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "G", "G", $"[{Units.Force}/{Units.Length}²]", GH_ParamAccess.item, new GH_Number(80760000));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "v", "v", "", GH_ParamAccess.item, new GH_Number(0.3));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+
+            evaluationUnit.RegisterInputParam(new Param_Number(), "Rho", "Rho", $"Density [{Units.Mass}/{Units.Length}³]", GH_ParamAccess.item, new GH_Number(7850));
+            evaluationUnit.Inputs[evaluationUnit.Inputs.Count - 1].Parameter.Optional = true;
+        }
+
+        public override void SolveInstance(IGH_DataAccess DA, out string msg, out GH_RuntimeMessageLevel level)
+        {
+            msg = "";
+            level = GH_RuntimeMessageLevel.Warning;
+
+            string matName = null;
+            double e = 210000000;
+            double eNeg = 210000000;
+            double eta = 0.00;
+            double g = 80760000;
+            double v = 0.3;
+            double rho = 7850;
+
+
+            DA.GetData(0, ref matName);
+            DA.GetData(1, ref e);
+            DA.GetData(2, ref eNeg);
+            DA.GetData(3, ref eta);
+            DA.GetData(4, ref g);
+            DA.GetData(5, ref v);
+            DA.GetData(6, ref rho);
+
+            //rho = rho * 9.81 / 1000;
+            var material = new Alpaca4d.Material.UniaxialMaterialElastic(matName, e, eNeg, eta, g, v, rho);
+
+            DA.SetData(0, material);
+        }
+
+    }
+}
---
+++ Alpaca4d.Gh/00_Material/nD/nDBase.cs
@@ -0,0 +1,108 @@
+﻿using Alpaca4d.UIWidgets;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using Grasshopper.Kernel.Special;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using Alpaca4d.UIWidgets;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alpaca4d.Gh;
+
+namespace SimplexGh
+{
+    public class nD : GH_SwitcherComponent
+    {
+        private List<SubComponent> _subcomponents = new List<SubComponent>();
+        public override string UnitMenuName => "nD";
+        protected override string DefaultEvaluationUnit => _subcomponents[0].name();
+        public override Guid ComponentGuid => new Guid("{B40E1124-E203-4E1A-8962-2A6FB8D751A5}");
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+
+        protected override Bitmap Icon => null;
+
+        public nD()
+            : base("nD", "nD",
+              "",
+              "Alpaca4d", "00_Material")
+        {
+            ((GH_Component)this).Hidden = true;
+            this.Message = this.Category;
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.RegisterParam(new Param_GenericObject(), "Material", "Material", "Material");
+        }
+
+        protected override void RegisterEvaluationUnits(EvaluationUnitManager mngr)
+        {
+            _subcomponents.Add();
+
+            foreach (SubComponent item in _subcomponents)
+            {
+                item.registerEvaluationUnits(mngr);
+            }
+        }
+
+        protected override void OnComponentLoaded()
+        {
+            base.OnComponentLoaded();
+            foreach (SubComponent item in _subcomponents)
+            {
+                item.OnComponentLoaded();
+            }
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA, EvaluationUnit unit)
+        {
+            if (unit == null)
+            {
+                return;
+            }
+            foreach (SubComponent item in _subcomponents)
+            {
+                if (unit.Name.Equals(item.name()))
+                {
+                    item.SolveInstance(DA, out var msg, out var level);
+                    if (msg != "")
+                    {
+                        ((GH_ActiveObject)this).AddRuntimeMessage(level, msg);
+                    }
+                    return;
+                }
+            }
+            throw new Exception("Invalid sub-component");
+        }
+        // Part of the code that allows to extend the menu with additional items
+        // Right click on the component to see the options
+        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalMenuItems(menu);
+        }
+        private void Menu_ActivateUnit(object sender, EventArgs e)
+        {
+            try
+            {
+                EvaluationUnit evaluationUnit = (EvaluationUnit)((ToolStripMenuItem)sender).Tag;
+                if (evaluationUnit != null)
+                {
+                    SwitchUnit(evaluationUnit);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
